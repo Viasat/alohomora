da6795f8c3438cf431eb9cf1b36362f688a3974b
Stephan Kemper <stephan.kemper@viasat.com>
2018-05-01 10:14:19 -0700

    Rev to 1.2.1 with --duration feature

--- alohomora/__init__.py
+++ alohomora/__init__.py
@@ -23,7 +23,7 @@ try:
 except NameError:
     pass
 
-__version__ = '1.2.0'
+__version__ = '1.2.1'
 __author__ = 'Stephan Kemper'
 __license__ = '(c) 2017 ViaSat, Inc. See the LICENSE file for more details.'
 
