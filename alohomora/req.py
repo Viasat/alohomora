2cebeee1a2393a7aae57dabc7bb39227d7f5af1f
Stephan Kemper <stephan.kemper@viasat.com>
2018-08-07 09:21:35 -0700

    Update copyright

--- alohomora/req.py
+++ alohomora/req.py
@@ -1,6 +1,6 @@
 """The workhorse functions that make web requests."""
 
-# Copyright 2017 ViaSat, Inc.
+# Copyright 2018 Viasat, Inc.
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
