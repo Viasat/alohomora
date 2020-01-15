86b7a73f17a4f53064d84a8a58b85e31fd4dbf53
Stephan Kemper <stephan.kemper@viasat.com>
2020-01-14 16:00:30 -0800

    Update copyright to 2020

--- alohomora/keys.py
+++ alohomora/keys.py
@@ -1,6 +1,6 @@
 """Handles getting and saving AWS API keys"""
 
-# Copyright 2019 Viasat, Inc.
+# Copyright 2020 Viasat, Inc.
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
