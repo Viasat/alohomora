e83ab16b39965fe7cdb50c478ce1758f6caf41d3
Austin Hodge <18039844+AustinHodge4@users.noreply.github.com>
2026-07-20 12:02:29 -0700

    Fix for changes to the Duo API (#55)
    * add browser based flow
    
    * fix: duo request api changes
    
    * fix: browser support + docs
    
    ---------
    
    Co-authored-by: bcaselden-viasat <robert.caselden@viasat.com>

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(
         "requests>=2.11.1",
     ],
     extras_require={
-        "fido2": ["fido2==0.9.3"]
+        "fido2": ["fido2==0.9.3"],
+        "browser": ["selenium>=4.0.0"],
     }
 )
